21b8c28bbd0749818dd3535516976717e0667fdf
ozgerd <130622398+ozgerd@users.noreply.github.com>
2023-05-09 15:39:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+
+let firstName = 'Arthur'
+let lastName = 'Dent'
+let country = 'UK'
+let city = 'London'
+let age = 100
+let isTeacher = true
+console.log (firstName, lastName, country, city, age, isTeacher)
+
+let fName = 'Tricia', surname = 'McMillan',  job = 'intergalactic traveller', live = 'UK'
+console.log (fName, surname, job,live)
+
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14
+console.log(gravity,boilingPoint, PI)
+
+let saying = 'Comments', todo = 'can', rdo = 'make', wr = 'coding', rd = 'fun'
+console.log(saying, todo, rdo, wr, rd )
+
+let secondExercise = 'Welcome to 30DaysOfJavaScript'
+console.log (secondExercise)
+
+let firstWord = 'Comments'
+let secondWord = 'can'
+let thirdWord = 'make'
+let fourthWord = 'code'
+let fifthWord = 'readable,'
+let sixthWord = 'easy to reuse'
+let seventhWord = 'and informative.'
+console.log (firstWord, secondWord, thirdWord, fourthWord, fifthWord, sixthWord, seventhWord)
+
+//string, boolean, undefined and null data types
+
+let testString = 'Marvin'
+let isRobot = true
+let Zaphod = undefined
+let isThere = null
+console.log (testString, isRobot, Zaphod, isThere)
+
+let nName = 'Zaphod'
+let nLastName = 'Beeblebrox'
+let isHere = true
+let place = 'Betelgeuse'
+let isYearKnown = undefined
+console.log (nName, nLastName, isHere, place, isYearKnown)
+
+let nimi = 'Zaphod', sukunimi = 'Beeblebrox', isEathian = 'false', star = 'Betelgeuse', isAgeKnown = null
+console.log (nimi, sukunimi, isEathian, star, isAgeKnown)
+
+let myAge = 'I am _ years old.'
+let yourAge = 'You are _ years old.'
+console.log (myAge, yourAge)
+
+/*
+Write here comments 
+*/
+
+console.log ('here', 'there', 'now')
+
+let here = 'yesterday'
+let there = 'tomorrow'
+let now = 'today'
+console.log (here, there, now)
